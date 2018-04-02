bb64069e632bcee1b90ee88d8cb82b544e4f06b9
Craig Rueda <craig.rueda@anaplan.com>
2018-04-02 16:43:16 +0100

    Added programs and contact slides

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ const $ = require('jquery');
 import Icon from './images/fam.jpg'; // This is needed to force webpack to include unreferenced files
 
 $(() => {
+  $('#copyYear').html(new Date().getFullYear()); // Keep the copyright up to date...
+
   $('#fullpage').fullpage({
-    anchors: [ 'main', 'why', 'about' ],
+    anchors: [ 'main', 'why', 'programs', 'about' ],
     css3: true,
     slidesNavigation: false,
-    verticalCentered: false
+    verticalCentered: false,
+    onLeave: (index, nextIndex, direction) => {
+      $('#toTop')[nextIndex == 1 ? 'hide' : 'show']();
+    }
   });
 });
