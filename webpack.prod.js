2dbe37c4856431fe6306ab9cae92907c73f12168
Craig Rueda <craig.rueda@anaplan.com>
2018-04-02 14:59:11 +0100

    Added CSS minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
    plugins: [
+     new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: { discardComments: { removeAll: true } }
+    }),
      new UglifyJsPlugin()
    ]
  });
